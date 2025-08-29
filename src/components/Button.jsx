56ffb6bbf0c3aa6f290a86ab6aa74c4deaced0ee
Chinmay Daroliya <chinmaydaroliya@gmail.com>
2025-08-29 12:07:27 +0530

    created text part of hero and button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,14 @@
-const Button = () => {
+const Button = ({text,className,id}) => {
     return (
-        <a className="cta-wrapper">
-            <div className="cta-group group">
-                <div className="bg-circle">
-                    <p className="text">See my work</p>
+       <a className={`${className ?? ''} cta-wrapper`}>
+            <div className="cta-button group">
+                <div className="bg-circle"></div>
+                    <p className="text">{text}</p>
                     <div className="arrow-wrapper">
-                        <img src="/images/arrow-down.svg" alt="arrow"/>
+                        <img src="/images/arrow-down.svg" alt="arrow" />
                     </div>
-                </div>
             </div>
-        </a>
+       </a>
     )
 }
 
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,63 @@
 import React from 'react';
+import { words } from '../constants';
+import Button from '../components/Button';
 
 const Hero = () => {
     return (
-        <div>
-            Hero
+       <section id='hero' className='relative overflow-hidden'>
+
+        <div className='absolute left-0 top-0 z-10'>
+            <img src='/images/bg.png' alt='hero-bg'/>
         </div>
+
+        <div className='hero-layout'>
+            {/* Left hero content */}
+            <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+                <div className='flex flex-col gap-7'>
+                <div className='hero-text'>
+                    <h1>
+                        Shading
+                        <span className='slide'>
+                            <span className='wrapper'>
+                                {words.map((word,index)=>(
+                                    
+                                    <span key={index} className='flex items-center md:gap-3 gap-1 pb-2'>
+                                        <img 
+                                            src={word.imgPath} 
+                                            alt="person"
+                                            className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50' 
+                                        />
+                                        <span>{word.text}</span>
+                                    </span>
+
+                                ))}
+                            </span>
+                        </span>
+                    </h1>
+                    <h1>into Real Projects</h1>
+                    <h1>that Deliver Results</h1>
+                </div>
+
+                <p className='text-white-50 md:text-xl relative z-10 pointer-events-none'>
+                    Hi, I’m Chinmay, a developer based in India with a passion for crafting clean, modern code             
+                </p>
+
+                <Button
+                    text="See My Work"
+                    className="md:w-80 md:h-16 w-60 h-12"
+                    id="counter"
+                    
+                />
+                </div>
+            </header>   
+            
+            {/* 3D modal */}
+            <figure>
+
+            </figure>
+        </div>
+
+       </section>
     )
 }
 
