f3d3e3ede4be8bd2ea3619b9c65e00c7579a4333
Chinmay Daroliya <chinmaydaroliya@gmail.com>
2025-09-01 14:51:17 +0530

    created feature cards section

--- src/constants/index.js
+++ src/constants/index.js
@@ -39,8 +39,28 @@ const navLinks = [
 
 ];
 
+const abilities = [
+  {
+    imgPath: "/images/seo.png",
+    title: "Full-Stack Foundations",
+    desc: "Skilled in Java, JavaScript, React, and Next.js with a focus on building scalable and user-friendly applications.",
+  },
+  {
+    imgPath: "/images/chat.png",
+    title: "Problem-Solving Mindset",
+    desc: "Passionate about tackling complex coding challenges and crafting efficient solutions through clean, maintainable code.",
+  },
+  {
+    imgPath: "/images/time.png",
+    title: "Continuous Learner",
+    desc: "Constantly exploring new technologies like AI/ML , Cloud Computing to stay adaptable in a fast-changing tech landscape.",
+  },
+];
+
+
 export {
   words,
   counterItems,
   navLinks,
+  abilities,
 };
